239a4c9e68d74ce641a7b1c38b37470518498cfd
riyagupta2011 <riyagupta75751@gmail.com>
2023-09-19 16:11:44 +0530

    completed backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ const express=require('express')
 const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const app=express();
-
+const pinRoute=require("./routes/pins")
+const userRoute=require("./routes/user")
 dotenv.config();
+
+app.use(express.json())
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true,}).then(()=>{
     console.log("Mongo Db connected");
 }).catch(err=>console.log(err))
+
+app.use("/api/pins",pinRoute);
+app.use("/api/users",userRoute);
 app.listen(8800,()=>{
     console.log("Backend server is running")
 })
